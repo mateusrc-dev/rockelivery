e87459fa3dfb7f82de379dc1a51b714ff12dc6c4
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-13 13:19:37 -0300

    updated project

--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -1,25 +1,7 @@
 defmodule Rockelivery.Users.Get do
   alias Rockelivery.{Error, User, Repo}
-  alias Ecto.UUID
-
-  def by_id_2(id) do
-    with {:ok, uuid} <- UUID.cast(id),
-         %User{} = user <- Repo.get(User, uuid) do
-      {:ok, user}
-    else
-      :error -> Error.build_id_format_error()
-      nil -> {:error, Error.build_user_not_found_error()}
-    end
-  end
 
   def by_id(id) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_error()}
-      {:ok, uuid} -> get(uuid)
-    end
-  end
-
-  defp get(id) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
