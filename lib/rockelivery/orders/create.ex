9c2f8fa6d9fa09707261ba761086b84f70a2a638
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-20 09:36:05 -0300

    create validation of items

---
+++ lib/rockelivery/orders/create.ex
@@ -0,0 +1,27 @@
+defmodule Rockelivery.Order.Create do
+  import Ecto.Query
+  alias Rockelivery.{Error, Item, Order, Repo}
+
+  def call(params) do
+    params
+    |> fetch_items()
+  end
+
+  defp fetch_items(%{"items" => items_params}) do
+    items_ids = Enum.map(items_params, fn item -> item["id"] end)
+
+    query = from item in Item, where: item.id in ^items_ids
+
+    query
+    |> Repo.all()
+    |> validate_items(items_ids)
+  end
+
+  defp validate_items(items, items_ids) do
+    items_map = Map.new(items, fn item -> {item.id, item} end)
+
+    items_ids
+    |> Enum.map(fn id -> {id, Map.get(items_map, id)} end)
+    |> Enum.any?(fn {_id, value} -> is_nil(value) end)
+  end
+end
