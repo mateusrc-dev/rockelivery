9526ec3ff0fdb8c238f8b4476ea1b35b1efa92a8
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-23 15:12:45 -0300

    updated project

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -4,13 +4,13 @@ defmodule Rockelivery.Factory do
 
   def user_params_factory do
     %{
-      age: 27,
-      address: "Rua das bananeiras, 15",
-      cep: "12345678",
-      cpf: "12345678901",
-      email: "mateus@bananas.com",
-      password: "123456",
-      name: "Mateus"
+      "age" => 27,
+      "address" => "Rua das bananeiras, 15",
+      "cep" => "12345678",
+      "cpf" => "12345678901",
+      "email" => "mateus@bananas.com",
+      "password" => "123456",
+      "name" => "Mateus"
     }
   end
 
