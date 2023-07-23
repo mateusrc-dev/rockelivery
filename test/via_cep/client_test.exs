7ea764415eca460c04710b0864a95b04c41bc009
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-23 12:23:49 -0300

    updated file client_test

--- test/via_cep/client_test.exs
+++ test/via_cep/client_test.exs
@@ -1,6 +1,8 @@
 defmodule Rockelivery.ViaCep.ClientTest do
   use ExUnit.Case, async: true
   alias Rockelivery.ViaCep.Client
+  alias Plug.Conn
+  alias Rockelivery.Error
 
   describe "get_cep_info/1" do
     setup do
@@ -32,8 +34,8 @@ defmodule Rockelivery.ViaCep.ClientTest do
       # this expect bypass is a mock
       Bypass.expect(bypass, "GET", "#{cep}/json/", fn conn ->
         conn
-        |> Plug.Conn.put_resp_header("content-type", "application/json")
-        |> Plug.Conn.resp(200, body)
+        |> Conn.put_resp_header("content-type", "application/json")
+        |> Conn.resp(200, body)
       end)
 
       response = Client.get_cep_info(url, cep)
@@ -51,6 +53,76 @@ defmodule Rockelivery.ViaCep.ClientTest do
            "logradouro" => "Praça da Sé",
            "siafi" => "7107",
            "uf" => "SP"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when the cep is invalid, returns an error", %{bypass: bypass} do
+      cep = "123"
+      url = endpoint_url(bypass.port)
+
+      # requests in Bypass occur in localhost - over it, we will have that use a url different
+      # this expect bypass is a mock
+      Bypass.expect(bypass, "GET", "#{cep}/json/", fn conn ->
+        Conn.resp(conn, 400, "")
+      end)
+
+      response = Client.get_cep_info(url, cep)
+
+      expected_response =
+        {:error,
+         %Error{
+           status: :bad_request,
+           result: "CEP is invalid!"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when the cep was not found, returns an error", %{bypass: bypass} do
+      cep = "00000000"
+
+      body = ~s({"erro": true})
+
+      url = endpoint_url(bypass.port)
+
+      # requests in Bypass occur in localhost - over it, we will have that use a url different
+      # this expect bypass is a mock
+      Bypass.expect(bypass, "GET", "#{cep}/json/", fn conn ->
+        conn
+        |> Conn.put_resp_header("content-type", "application/json")
+        |> Conn.resp(200, body)
+      end)
+
+      response = Client.get_cep_info(url, cep)
+
+      expected_response =
+        {:error,
+         %Error{
+           status: :not_found,
+           result: "CEP not found!"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when there is a generic error, returns an error", %{bypass: bypass} do
+      cep = "00000000"
+
+      url = endpoint_url(bypass.port)
+
+      # requests in Bypass occur in localhost - over it, we will have that use a url different
+      # down will close server just in this test
+      Bypass.down(bypass)
+
+      response = Client.get_cep_info(url, cep)
+
+      expected_response =
+        {:error,
+         %Error{
+           status: :bad_request,
+           result: :econnrefused
          }}
 
       assert response == expected_response
