d1c969dad51b5e19ac0b99f8dd3dedcf0a793319
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-25 21:20:39 -0300

    updated file guardian - created function of authenticate

--- lib/rockelivery_web/auth/guardian.ex
+++ lib/rockelivery_web/auth/guardian.ex
@@ -1,6 +1,6 @@
 defmodule RockeliveryWeb.Auth.Guardian do
   use Guardian, otp_app: :rockelivery
-  alias Rockelivery.User
+  alias Rockelivery.{Error, User}
   alias Rockelivery.Users.Get, as: UserGet
 
   # function that will defined the 'sub' - 'sub' will be defined with id of user
@@ -11,5 +11,20 @@ defmodule RockeliveryWeb.Auth.Guardian do
   def resource_from_claims(%{"sub" => id}) do
     id
     |> UserGet.by_id()
+  end
+
+  def authenticate(%{"id" => user_id, "password" => password}) do
+    with {:ok, %User{password_hash: hash} = user} <- UserGet.by_id(user_id),
+         true <- Pbkdf2.verify_pass(password, hash),
+         {:ok, token, _claims} <- encode_and_sign(user) do
+      {:ok, token}
+    else
+      false -> {:error, Error.build(:unauthorized, "Please verify your credentials")}
+      error -> error
+    end
+  end
+
+  def authenticate(_) do
+    {:error, Error.build(:bad_request, "Invalid or missing params")}
   end
 end
