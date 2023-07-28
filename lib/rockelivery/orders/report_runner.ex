33ab8cce81a7c1a6e9432a9a045cc66d2f3b6203
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-28 13:15:55 -0300

    created report runner

---
+++ lib/rockelivery/orders/report_runner.ex
@@ -0,0 +1,33 @@
+defmodule Rockelivery.Orders.ReportRunner do
+  use GenServer
+  alias Rockelivery.Orders.Report
+
+  require Logger
+
+  # client
+  def start_link(_initial_stack) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  # server
+  @impl true
+  def init(state) do
+    schedule_report_generation()
+    {:ok, state}
+  end
+
+  @impl true
+  # receive anything of message
+  def handle_info(:generate, state) do
+    Logger.info("Generating report...")
+
+    schedule_report_generation()
+    Report.create()
+
+    {:noreply, state}
+  end
+
+  def schedule_report_generation do
+    Process.send_after(self(), :generate, 1000 * 60)
+  end
+end
